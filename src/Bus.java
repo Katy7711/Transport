25053bf4f2bde5d18b6316919949eda0ed4ef401
Katy7711 <deniev777@yandex.ru>
2022-10-23 19:57:34 +0500

    Homework

---
+++ src/Bus.java
@@ -0,0 +1,11 @@
+public class Bus extends Transport {
+    public Bus(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed) {
+        super(brand, model, productionYear, productionCountry, color, maxSpeed);
+    }
+
+    @Override
+    public void refill() {
+        System.out.println("Автобусы можно заправлять бензином или дизелем на заправке");
+
+    }
+}
---
+++ src/Transport.java
@@ -0,0 +1,83 @@
+public abstract class Transport {
+
+    private String brand;
+    private String model;
+    private final int productionYear;
+    private final String productionCountry;
+    private String color;
+    private int maxSpeed;
+
+
+    public Transport(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed) {
+        if (brand == null || brand.isEmpty() || brand.isBlank())
+            brand = "default";
+        this.brand = brand;
+        if (model == null || model.isEmpty() || model.isBlank())
+            model = "default";
+        this.model = model;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+        if (color == null || color.isEmpty() || color.isBlank())
+            color = "цвет не указан";
+        this.color = color;
+        if (maxSpeed <= 0)
+            maxSpeed = 100;
+        this.maxSpeed = maxSpeed;
+        }
+
+
+    public abstract void refill ();
+
+
+    public String getBrand() {
+            return brand;
+        }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+            this.model = model;
+        }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "brand='" + "brand='" + getBrand() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", productionYear=" + getProductionYear() +
+                ", productionCountry='" + getProductionCountry() + '\'' +
+                ", color='" + getColor() + '\'' +
+                ", maxSpeed=" + getMaxSpeed() +
+                '}';
+    }
+}
